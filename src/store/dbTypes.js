0fa74bab7157a2e32e7c12e34ac672d71250033f
Kate Lysychenko <e.lysychenko@teamjet.pro>
2019-11-17 21:10:03 +0300

    ref: clean

--- src/store/dbTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DB;
-(function (DB) {
-    DB.storeURLs = 'URLs';
-    DB.storeUsers = 'Users';
-    DB.storeCounter = 'Counter';
-})(DB = exports.DB || (exports.DB = {}));
